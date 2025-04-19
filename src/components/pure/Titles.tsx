8dd39276765ba474138a2d63720f68aa8d0fce63
Victor Medina <mijaresaugusto058@gmail.com>
2025-04-19 11:51:38 -0400

    le di estilos al componente Titles.tsx y lo integre con el componente SectionEntrenamiento.tsx y agregue un tipografia al index.css

--- src/components/pure/Titles.tsx
+++ src/components/pure/Titles.tsx
@@ -1,6 +1,10 @@
-const Titles = () => {
+type TitlesProps = {
+  title: string;
+}
+
+const Titles = ({title}:TitlesProps) => {
   return (
-    <h1>hols</h1>
+    <h1 className={' capitalize font-[Niramit] tracking-widest text-[18px] border-b-2 font-normal border-(--color-gold) '} >{title}</h1>
   )
 }
 
--- src/components/sections/SectionEntrenamiento.tsx
+++ src/components/sections/SectionEntrenamiento.tsx
@@ -1,11 +1,15 @@
+import Titles from "../pure/Titles.tsx";
+
 type SectionHomeProps = {
   idName: string;
 }
 
 const SectionHome = ({idName}:SectionHomeProps) => {
   return (
-    <section id={idName} >
-      This is the section entrenamiento
+    <section id={idName} className={' h-screen w-full pt-10 '}  >
+      <div className={' w-full flex items-center justify-center '} >
+        <Titles title={'entrenamientos'}/>
+      </div>
     </section>
   )
 }
