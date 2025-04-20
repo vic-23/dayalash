c21d8937c68d716ac7600c91d56fab8c8974b3aa
Victor Medina <mijaresaugusto058@gmail.com>
2025-04-19 20:50:10 -0400

    Informacion de los cursos en formato de array

---
+++ src/data/cursos.js
@@ -0,0 +1,77 @@
+export const courses = [
+  {
+    id: 1,
+    title: 'entrenamiento inicial',
+    description: 'Adquiere todo el conocimiento teórico y práctico para convertirte en una experta en extensiones de pestañas',
+    features: [
+      {
+        id: 1,
+        text: 'teoría especializada sobre pestañas'
+      },
+      {
+        id: 2,
+        text: 'técnica y aplicación de extensiones'
+      },
+      {
+        id: 3,
+        text: 'práctica supervisada bajo guía profesional'
+      },
+      {
+        id: 4,
+        text: 'manejo de cliente y emprendimiento'
+      }
+    ],
+    duration: '2 semanas',
+    methodology: 'online'
+  },
+  {
+    id: 2,
+    title: 'entrenamiento avanzado',
+    description: 'lleva tu técnica al siguiente nivel y ofrece un servicio más preciso, duradero y personalizado',
+    features: [
+      {
+        id: 1,
+        text: 'Perfecciona tu aislamiento y retención'
+      },
+      {
+        id: 2,
+        text: 'domina efectos como el wet look , volumen, híbrido y mapping avanzado '
+      },
+      {
+        id: 3,
+        text: 'optimiza tu velocidad y técnica',
+      },
+      {
+        id: 4,
+        text: 'aprende a corregir errores y personalizar tus diseños'
+      }
+    ],
+    duration: '2 semanas',
+    methodology: 'online'
+  },
+  {
+    id: 3,
+    title: 'webinar exclusivo',
+    description: 'lleva tu técnica al siguiente nivel y ofrece un servicio más preciso, duradero y personalizado',
+    features: [
+      {
+        id: 1,
+        text: 'Perfecciona tu aislamiento y retención'
+      },
+      {
+        id: 2,
+        text: 'domina efectos como el wet look , volumen, híbrido y mapping avanzado '
+      },
+      {
+        id: 3,
+        text: 'optimiza tu velocidad y técnica',
+      },
+      {
+        id: 4,
+        text: 'aprende a corregir errores y personalizar tus diseños'
+      }
+    ],
+    duration: '2 semanas',
+    methodology: 'online'
+  }
+];
