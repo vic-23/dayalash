2f14aa9bf52f2b06b1d74f8ff518411e1127bc8f
Victor Medina <mijaresaugusto058@gmail.com>
2025-04-26 19:58:20 -0400

    Se ajusto el diseño

--- src/components/sections/SectionEntrenamiento.tsx
+++ src/components/sections/SectionEntrenamiento.tsx
@@ -23,8 +23,8 @@ const SectionHome = ({idName}:SectionHomeProps) => {
   return (
     <section id={idName} className={' h-auto w-full pt-10 '}  >
       <div className={' w-full flex flex-col items-center justify-center '} >
-        <Titles title={'entrenamientos'}/>
-        <h1 className="font-[Niramit-bold] text-[clamp(1.8rem,9vw,4rem)] text-center">
+        <Titles title={'Entrenamientos'}/>
+        <h1 className='font-[Niramit-bold] text-[clamp(1.8rem,9vw,4rem)] text-center'>
           Aprende en Dayalash Academy
         </h1>
         <h2 className={' flex flex-col font-[Niramit-SemiBold] text-center text-[clamp(0.950rem,3vw,5rem)] '} >
