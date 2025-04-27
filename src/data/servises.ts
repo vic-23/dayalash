6b3b412c9c035d4c406d9dcabdc68492eb50ed84
Victor Medina <mijaresaugusto058@gmail.com>
2025-04-26 20:14:26 -0400

    Estructura de la data para los servicio que se ofrecen.

--- src/data/servises.ts
+++ src/data/servises.ts
@@ -1,5 +1,28 @@
+import eyelashes from '../assets/eyelashes.svg'
+
 export const servises = [
   {
-    id
+    id: 1,
+    icon: eyelashes,
+    title: 'Clásicas',
+    description: 'Efecto natural, pelo a pelo',
+  },
+  {
+    id: 2,
+    icon: eyelashes,
+    title: 'Wett Effect',
+    description: 'Efecto natural, pelo a pelo',
+  },
+  {
+    id: 3,
+    icon: eyelashes,
+    title: 'Volumen Ruso',
+    description: 'Efecto natural, pelo a pelo',
+  },
+  {
+    id: 4,
+    icon: eyelashes,
+    title: 'Volumen Tecnológico',
+    description: 'Efecto natural, pelo a pelo',
   }
 ]
